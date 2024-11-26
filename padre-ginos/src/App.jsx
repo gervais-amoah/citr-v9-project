c81b6e457f17946a5547663fc013731910d327dd
gervais <gervaisamoah@yahoo.com>
2024-11-26 12:19:58 +0000

    Pizza of the day

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,13 +1,15 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1>Padre Gino's - Order Now</h1>
+        <h1 className="logo">Padre Gino's - Order Now</h1>
         <Order />
+        <PizzaOfTheDay />
       </div>
     </StrictMode>
   );
